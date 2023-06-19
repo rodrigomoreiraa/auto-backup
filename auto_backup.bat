b350d5ff89c4aaa7d7ca7dfff784c527a916c47a
Paulo <paulo.rodrigo@cmprn.local>
2023-06-19 13:35:29 -0300

    nova atualização do arquivo

--- auto_backup.bat
+++ auto_backup.bat
@@ -1,12 +1,27 @@
 @echo off
 
+setlocal enabledelayedexpansion
+
+REM Exibir unidades locais e USB disponíveis
+echo Unidades disponiveis:
+wmic logicaldisk get caption
+
+REM Solicitar ao usuário a letra da unidade de origem
+set /p source_letter=Digite a letra da unidade de origem: 
+
+REM Solicitar ao usuário a letra da unidade de destino
+set /p destination_letter=Digite a letra da unidade de destino: 
+
+REM Solicitar o usuário deretório de origem
 set /p source_directory=Insira a origem:
+
+REM Solicitar o usuário diretório de origem
 set /p destination_directory= Agora insira o destino do backup:
 
 echo Copiando arquivos e pastas...
 
 xcopy /E /C /I /Q /Y "%source_directory%" "%destination_directory%" 
 
-echo Copiar e colar conclu�dos.
+echo Copiar e colar concluidos.
 
 pause
